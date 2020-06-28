0044f08f71c4bfe6ee1edfed25c0b5be04e598d5
Flora Petersen <floramarina@comcast.net>
2020-06-28 19:57:53 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,9 +3,9 @@ class Appointment
 
   @@all = []
 
-  def initialize(date, patient, doctor)
-    @date = date
+  def initialize(patient, date, doctor)
     @patient = patient
+    @date = date
     @doctor = doctor
     @@all << self
   end
